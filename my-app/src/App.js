965de9bb0a5055f454343495db319453f93c2bc1
binhminhst <binhminhst@gmail.com>
2017-05-03 18:42:24 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import './Customer.css';
+import './Customer.css';  
+ 
+const { render } = ReactDOM; 
 
+/*
 var data = [
 	{
 		"ID": 1,
@@ -38,11 +41,40 @@ var data = [
 		"DESC": "CAMEO-SILK--A THOUSAND LIGHTS",
 		"PRICE": "810000"  	 	
 	}
-];
+]; */
+
+
  
-class APP_show_json extends React.Component{
-	render() {
-		    var row_table ;
+class APP_show_json extends React.Component{ 
+	
+	constructor(props) {
+        super(props);
+        this.state = { persons: [] };
+    }
+    
+     componentDidMount() {
+		 
+        fetch('http://localhost/learning_laravel_api/api/public/api/shop/show',{
+			 
+				//mode: 'no-cors',
+				method: 'get',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json', 
+				},
+				
+		}).then((data) => {
+				 this.setState({persons: data.results })  
+				console.log('AAA', data.result );
+				  
+		}).catch(function (ex) {
+				console.log('BBB', ex);
+        }) 
+		 
+    }   
+	 
+	 render() {
+		
 			return(
 				<div className="body-page">
 					<div className="title-page">Example First About ReactJS</div>
@@ -55,30 +87,42 @@ class APP_show_json extends React.Component{
 							<div className="col_table">TYPE</div>
 							<div className="col_table">MISA_CODE</div>
 							<div className="col_table">DESC</div>
-							<div className="col_table">PRICE</div>
-						</div>
+							<div className="col_table">PRICE</div> 
+						</div> 
+						 
 					</div>
 				</div>
-			);
-		
-	};
+			); 
+			 
+	}   	
 	
-/*	getRows: function() {
-			var data = this.props.data;
-
-			return data.map(function(item) {				  
-				return  <div className="header_table" key={item.id}>
-							<div className="col_table">{item.id}</div>
-							<div className="col_table">FAB_RIC</div>
-							<div className="col_table">FAMILY</div>
-							<div className="col_table">TYPE</div>
-							<div className="col_table">MISA_CODE</div>
-							<div className="col_table">DESC</div>
-							<div className="col_table">PRICE</div>
-						</div> 
-			});
-	} 
-*/	
 }
+		 
+	  /*	var	Rows = data.map(function(item) {	 
+				return  <div className="header_table, clear_both" key={item.ID}>
+							<div className="col_table">{item.ID}</div>
+							<div className="col_table">{item.FAB_RIC}</div>
+							<div className="col_table">{item.FAMILY}</div>
+							<div className="col_table">{item.TYPE}</div>
+							<div className="col_table">{item.MISA_CODE}</div>
+							<div className="col_table">{item.DESC}</div>
+							<div className="col_table">{item.PRICE}</div> 
+						</div>
+										
+		}); 
+		
+		 
+		var Rows = this.state.persons.map(function(item){
+				return  <div className="header_table, clear_both" key={item.id}>
+							<div className="col_table">{item.id}</div>
+							<div className="col_table">{item.fab_ric}</div>
+							<div className="col_table">{item.family}</div>
+							<div className="col_table">{item.type}</div>
+							<div className="col_table">{item.misa_code}</div>
+							<div className="col_table">{item.desc}</div>
+							<div className="col_table">{item.price}</div> 
+						</div>
+										
+		}); */
 
 export default APP_show_json ;
